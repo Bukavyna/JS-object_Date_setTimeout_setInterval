a611c1fb4b17266e30c722815bcb8e9785f90b7f
Taras <luchakt2@gmail.com>
2023-12-30 17:26:01 +0200

    added mechanical-watch

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+function rotateClockHands() {
+	const currentTime = new Date();
+	const seconds = currentTime.getSeconds();
+	const minutes = currentTime.getMinutes();
+	const hours = currentTime.getHours() % 12;
+
+	const secondHand = document.querySelector('.second-hand');
+	const minuteHand = document.querySelector('.minute-hand');
+	const hourHand = document.querySelector('.hour-hand');
+
+	const secondRotation = seconds * 6;
+	const minuteRotation = (minutes * 6) + (seconds * 0.1);
+	const hourRotation = (hours * 30) + (minutes * 0.5);
+
+	secondHand.style.transform = `translateX(-50%) rotate(${secondRotation}deg)`;
+	minuteHand.style.transform = `translateX(-50%) rotate(${minuteRotation}deg)`;
+	hourHand.style.transform = `translateX(-50%) rotate(${hourRotation}deg)`;
+}
+
+setInterval(rotateClockHands, 1000);
+rotateClockHands();
